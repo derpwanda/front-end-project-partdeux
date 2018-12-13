acafa626c2ce2155bd7ec50caf767bbc2a3a03a3
Wanda <doubleuevans@gmail.com>
2018-12-12 19:25:31 -0600

    added redux dependencies; prepped create-react-app

--- lambda-notes/src/index.js
+++ lambda-notes/src/index.js
@@ -3,8 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {BrowserRouter as Router} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const dummyReducer = (state=[], action) => state;
+
+const store = createStore(dummyReducer)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>
+  ,document.getElementById('root')
+  );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
