e6f7d1f275ea1cefd1d6be218877d647c089ecc0
Wanda <doubleuevans@gmail.com>
2018-12-12 20:46:33 -0600

    styled buttons and navbar

--- lambda-notes/src/components/SideNav/SideNav.js
+++ lambda-notes/src/components/SideNav/SideNav.js
@@ -4,8 +4,15 @@ import './index.css';
 class SideNav extends Component {
   render() {
     return (
-      <div className='sidenav'>
-        SIDE NAV
+      <div className='side_nav'>
+        <div>
+          <h1 className='nav_header'>Lambda Notes</h1>
+        </div>
+
+        <div className='nav_buttons_container'>
+          <div className="nav_button">View Your Notes</div>
+          <div className="nav_button">+ Create New Note</div>
+        </div>
       </div>
     );
   }
