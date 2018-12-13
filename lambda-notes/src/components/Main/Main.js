2b1f66417d27da4eb960dcaff20710683b263c30
Wanda <doubleuevans@gmail.com>
2018-12-12 21:28:05 -0600

    styled notelist

--- lambda-notes/src/components/Main/Main.js
+++ lambda-notes/src/components/Main/Main.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './index.css';
+import NotesList from '../NotesList/NotesList.js'
 
 class Main extends Component {
   render() {
     return (
-      <div className="main">
-        MAIN COMPONENT
+      <div className="main_container">
+        <NotesList />
       </div>
     );
   }
--- lambda-notes/src/components/NotesList/NotesList.js
+++ lambda-notes/src/components/NotesList/NotesList.js
@@ -4,8 +4,45 @@ import './index.css';
 class NotesList extends Component {
   render() {
     return (
-      <div>
-        NOTE LIST
+      <div className='noteslist_container'>
+        <div className="noteslist_header"><h3>Your Notes:</h3></div>
+        <div className='notes_list'>
+          <div className='note'>
+            <h4>Note Tile</h4>
+            <hr></hr>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sed ipsum quis velit dictum tristique in ac nisi. In hac habitasse platea dictumst. Ut consectetur, nunc nec blandit tristique, ex nisl faucibus orci, at efficitur quam justo nec eros. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ante ipsum.</p>
+          </div>
+
+          <div className='note'>
+            <h4>Note Tile</h4>
+            <hr></hr>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sed ipsum quis velit dictum tristique in ac nisi. In hac habitasse platea dictumst. Ut consectetur, nunc nec blandit tristique, ex nisl faucibus orci, at efficitur quam justo nec eros. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ante ipsum.</p>
+          </div>
+
+          <div className='note'>
+            <h4>Note Tile</h4>
+            <hr></hr>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sed ipsum quis velit dictum tristique in ac nisi. In hac habitasse platea dictumst. Ut consectetur, nunc nec blandit tristique, ex nisl faucibus orci, at efficitur quam justo nec eros. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ante ipsum.</p>
+          </div>
+
+          <div className='note'>
+            <h4>Note Tile</h4>
+            <hr></hr>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sed ipsum quis velit dictum tristique in ac nisi. In hac habitasse platea dictumst. Ut consectetur, nunc nec blandit tristique, ex nisl faucibus orci, at efficitur quam justo nec eros. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ante ipsum.</p>
+          </div>
+
+          <div className='note'>
+            <h4>Note Tile</h4>
+            <hr></hr>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sed ipsum quis velit dictum tristique in ac nisi. In hac habitasse platea dictumst. Ut consectetur, nunc nec blandit tristique, ex nisl faucibus orci, at efficitur quam justo nec eros. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ante ipsum.</p>
+          </div>
+          <div className='note'>
+            <h4>Note Tile</h4>
+            <hr></hr>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sed ipsum quis velit dictum tristique in ac nisi. In hac habitasse platea dictumst. Ut consectetur, nunc nec blandit tristique, ex nisl faucibus orci, at efficitur quam justo nec eros. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ante ipsum.</p>
+          </div>
+
+          </div>
       </div>
     );
   }
